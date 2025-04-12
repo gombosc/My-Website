c9b373db9113a39ecef69b4ae588a151923c2c94
gombosc <cosmingombos@gmail.com>
2025-04-12 19:34:28 +0200

    modified content security policy to allow giscus

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const ContentSecurityPolicy = `
   default-src 'self';
   script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app www.google-analytics.com www.googletagmanager.com;
-  style-src 'self' 'unsafe-inline';
+  style-src 'self' 'unsafe-inline' giscus.app;
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
   connect-src *;
